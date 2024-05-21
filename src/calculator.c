aa428278622cf91c85509f3d7c85bffd4a762038
KrsXishere <lxghozt28@gmail.com>
2024-05-21 14:10:50 +0700

    refactor: refactoring menu decision keyword using witch case

--- src/calculator.c
+++ src/calculator.c
@@ -112,74 +112,74 @@ void runCalc(){
 	scanf("%d",&pil);
     fflush(stdin);
     if((pil == 1 || pil == 2 || pil == 3 || pil == 4|| pil ==5)){
-		    if(pil==1){
-		        char expression[200];
-		        displayexpression:
-		        system("cls");
-		        TampilanKalkulator();
-				gotoxy(44,6);scanf("%s",expression);
-		        fflush(stdin);
-		        if(isValid(expression)){
-		        	gotoxy(65,25); printf("%s", expression);
-   			        struct calcTree* binaryTree=(struct calcTree *)malloc(sizeof(struct calcTree));
-			        binaryTree = createTree(expression,0,strlen(expression)-1);
-			        gotoxy(44,5); printf("Result                   ");
-			        gotoxy(44,6); printf("                                    \033[0;35m| |\033[0;37m\n");
-					gotoxy(44,6); printf("%f",count(binaryTree));
-			        gotoxy(59,26); infixToPostfix(binaryTree);
-			        gotoxy(59,27);infixToPrefix(binaryTree);
-			        gotoxy(40,28);printf("Hasil pembulatan dua angka dibelakang koma : %0.2f",round_double(count(binaryTree),2));
-				}else{
-					gotoxy(38,2); printf("Ekspresi Matematika Yang Anda Masukkan Salah!!!");
-				}
-		        tryAgain:
-		        gotoxy(44,30);printf("Apakah ingin mencoba lagi ? (Y/N):  ");
-		        gotoxy(44+35,30);scanf("%c", &cobaLagi);
-		        fflush(stdin);
-		        if(cobaLagi =='Y'||cobaLagi=='y'){
-		        	system("cls");
-		        	pil=0;
-		        	goto displayexpression;
-				}
-				else if(cobaLagi =='N'||cobaLagi=='n'){
-					system("cls");
-					pil=0;
-					goto AppMenu;
-				}
-				else{
-					gotoxy(38,33);
-					printf("Maaf, perintah yang Anda masukkan tidak dapat diproses.");
-					gotoxy(42,34);
-					printf("Mohon untuk memasukkan sesuai dengan instruksi.");
-					gotoxy(55,35);
-					printf("Terima kasih.");
-					pil=0;
-					goto tryAgain;
-				}
-			}
-			else if(pil==3){
-				mainMenuTemp();
-				pil=0;
-				goto AppMenu;
-			}
-			else if(pil==2){
+    	char expression[200];
+	    switch (pil) {
+	    	case 1:
+	    		displayexpression:
 				system("cls");
-				TampilanBatasanProgram();
-				system("cls");
-				pil=0;
-				goto AppMenu;
-			}
-			else if(pil==4){
-				aboutCalculator();
-				system("cls");
-				pil=0;
-				goto AppMenu;
-
-			}
-			else if(pil==5){
-				printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\nTerimakasih Telah Menggunakan ClevCalc");
-
-			}
+	    		TampilanKalkulator();
+	    		gotoxy(44,6);scanf("%s",expression);
+	    		fflush(stdin);
+	    		if(isValid(expression)){
+	    			gotoxy(65,25); printf("%s", expression);
+	    			struct calcTree* binaryTree=(struct calcTree *)malloc(sizeof(struct calcTree));
+	    			binaryTree = createTree(expression,0,strlen(expression)-1);
+	    			gotoxy(44,5); printf("Result                   ");
+	    			gotoxy(44,6); printf("                                    \033[0;35m| |\033[0;37m\n");
+	    			gotoxy(44,6); printf("%f",count(binaryTree));
+	    			gotoxy(59,26); infixToPostfix(binaryTree);
+	    			gotoxy(59,27);infixToPrefix(binaryTree);
+	    			gotoxy(40,28);printf("Hasil pembulatan dua angka dibelakang koma : %0.2f",round_double(count(binaryTree),2));
+	    		}else{
+	    			gotoxy(38,2); printf("Ekspresi Matematika Yang Anda Masukkan Salah!!!");
+	    		}
+	    		tryAgain:
+				gotoxy(44,30);printf("Apakah ingin mencoba lagi ? (Y/N):  ");
+	    		gotoxy(44+35,30);scanf("%c", &cobaLagi);
+	    		fflush(stdin);
+	    		if(cobaLagi =='Y'||cobaLagi=='y'){
+	    			system("cls");
+	    			pil=0;
+	    			goto displayexpression;
+	    		}
+	    		else if(cobaLagi =='N'||cobaLagi=='n'){
+	    			system("cls");
+	    			pil=0;
+	    			goto AppMenu;
+	    		}
+	    		else{
+	    			gotoxy(38,33);
+	    			printf("Maaf, perintah yang Anda masukkan tidak dapat diproses.");
+	    			gotoxy(42,34);
+	    			printf("Mohon untuk memasukkan sesuai dengan instruksi.");
+	    			gotoxy(55,35);
+	    			printf("Terima kasih.");
+	    			pil=0;
+	    			goto tryAgain;
+	    		}
+	    		break;
+	    	case 2:
+	    		system("cls");
+	    		TampilanBatasanProgram();
+	    		system("cls");
+	    		pil=0;
+	    		goto AppMenu;
+				break;
+	    	case 3:
+	    		mainMenuTemp();
+	    		pil=0;
+	    		goto AppMenu;
+				break;
+	    	case 4:
+	    		aboutCalculator();
+	    		system("cls");
+	    		pil=0;
+	    		goto AppMenu;
+				break;
+	    	case 5:
+	    		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\nTerimakasih Telah Menggunakan ClevCalc");
+	    		break;
+	    }
 	}else{
 		gotoxy(34,19); printf("Maaf, perintah yang Anda masukkan tidak dapat diproses.");
 		gotoxy(24,20); printf("Mohon untuk memilih menu dengan angka yang sesuai dengan opsi yang tersedia.");
